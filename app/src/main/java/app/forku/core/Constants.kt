151ffded8e9e212f43cc70f8f00627ec534f7cad
Daniel Durán Schütz <danissch@gmail.com>
2025-03-10 09:12:05 -0500

    New v2 api url

--- app/src/main/java/app/forku/core/Constants.kt
+++ app/src/main/java/app/forku/core/Constants.kt
@@ -1,7 +1,8 @@
 package app.forku.core
 
 object Constants {
-    const val BASE_URL = "https://67b8faf351192bd378dc5bdf.mockapi.io/forku/api/v1/"
+    const val BASE_URL = "https://67ce79d2125cd5af757aa258.mockapi.io/forku/api/v2/" //Dev
+    //const val BASE_URL = "https://67b8faf351192bd378dc5bdf.mockapi.io/forku/api/v1/" //Test
     const val API_VERSION = "v1"
 
 }
